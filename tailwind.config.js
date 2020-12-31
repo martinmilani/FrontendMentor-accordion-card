1ebde2a89eec8022cb858fce4cfbd6e15395ad2a
Martín <martin@pop-os.localdomain>
2020-12-31 17:50:22 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        veryDarkDesaturatedBlue: '#1d1e35',
+        softRed:'#f47c57',
+        veryDarkGrayishBlue:'#554a5e',
+        darkGrayishBlue:'#787887',
+        lightGrayishBlue:'#e7e7e9',
+        softViolet: '#af67e9',
+        softBlue: '#6565e7',
+      },
+      fontFamily: {
+        kumbhSans:['Kumbh Sans']
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
