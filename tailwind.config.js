9460528cbe4369d1a36d5e485363c91fc5e3f949
Martin Milani <martin.milani.mdq@gmail.com>
2021-01-04 00:08:35 -0300

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,33 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors:{
-        veryDarkDesaturatedBlue: '#1d1e35',
-        softRed:'#f47c57',
-        veryDarkGrayishBlue:'#554a5e',
-        darkGrayishBlue:'#787887',
-        lightGrayishBlue:'#e7e7e9',
-        softViolet: '#af67e9',
-        softBlue: '#6565e7',
+      height: {
+        bigSize: "200%",
+      },
+      colors: {
+        veryDarkDesaturatedBlue: "#1d1e35",
+        softRed: "#f47c57",
+        veryDarkGrayishBlue: "#554a5e",
+        darkGrayishBlue: "#787887",
+        lightGrayishBlue: "#e7e7e9",
+        softViolet: "#af67e9",
+        softBlue: "#6565e7",
       },
       fontFamily: {
-        kumbhSans:['Kumbh Sans']
-      }
-
+        kumbhSans: ["Kumbh Sans"],
+      },
+      fontSize: {
+        customXl: "2rem",
+      },
+      spacing: {
+        custom: "5.5rem",
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ["active"],
+    },
   },
   plugins: [],
-}
+};
